462cab65ade79f6fe638f62c7c81d1384e6d1577
DarshilChodvadiya <85396569+DarshilChodvadiya@users.noreply.github.com>
2021-06-06 19:55:42 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  var currYear = new Date().getFullYear;
+
+  return (
+    <div>
+      <Footer>
+        <p>Copyright @ {currYear} </p>
+      </Footer>
+    </div>
+  );
+}
+
+export default Footer;
+
